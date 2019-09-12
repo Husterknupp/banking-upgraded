9179d428e88b0be730bb463bd9c54c9202f5927a
bens <bens@spreadshirt.net>
2019-09-12 15:25:06 +0200

    meh.. delete unused import

--- client/banking-upgraded/src/App.tsx
+++ client/banking-upgraded/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default () => {
   const [documentResponse, setDocumentResponse] = useState<Response | null>(
